d1b099b60f976c975dcaa106b52ac9a67837f60a
Oswaldo Linhares <oswaldolinhares@gmail.com>
2015-12-04 12:07:33 -0300

    Mudança de Rota da HOMEPAGE

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#home'
   get 'pages/home', to: 'pages#home'
   get 'pages/about',to: 'pages#about'
 
